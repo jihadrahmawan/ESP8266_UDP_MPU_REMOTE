50469314a6693f6e44dd4b0f36dc9ed36fd5db72
Jihad Rahmawan <jihadrahmawan20@gmail.com>
2023-01-05 18:07:35 +0900

    Update UDP.cpp

--- UDP.cpp
+++ UDP.cpp
@@ -14,12 +14,16 @@ WiFiUDP wifiUdp;
 const char *kRemoteIpadr = "192.168.100.29";  //IP RASPI
 const int kRmoteUdpPort = 9000;               // PORT
 
+int PITCH = 0;
+int ROLL = 0;
 int PITCH_MAX = 0;
 int PITCH_MIN = 0;
 int ROLL_MAX = 0;
 int ROLL_MIN = 0;
 int last_PITCH = 0;
 int last_ROLL = 0;
+int m_roll = 0;
+int m_pitch = 0;
 bool isCalibrated = false;
 const int PIN_LED_FRONT = 5;  //D5
 const int PIN_LED_RIGHT = 8;  //D8
@@ -38,21 +42,25 @@ void setup() {
   pinMode(PIN_LED_RIGHT, OUTPUT);
   pinMode(PIN_LED_BACK, OUTPUT);
   pinMode(PIN_LED_LEFT, OUTPUT);
+  
   digitalWrite(PIN_LED_FRONT, LOW);
   digitalWrite(PIN_LED_RIGHT, LOW);
   digitalWrite(PIN_LED_BACK, LOW);
   digitalWrite(PIN_LED_LEFT, LOW);
-
+  digitalWrite(LED_BUILTIN, LOW);
+  
+  Serial.begin(115200);
   const int kLocalPort = 7000;  //ignore
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Koneksi ke wifi...");
     digitalWrite(LED_BUILTIN, LOW);
     delay(100);
     digitalWrite(LED_BUILTIN, HIGH);
     delay(100);
   }
   wifiUdp.begin(kLocalPort);
-  Serial.begin(57600);
+ 
   while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G)) {
     Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
     delay(500);
@@ -66,32 +74,45 @@ void setup() {
 void loop() {
 
   Vector normAccel = mpu.readNormalizeAccel();
-  int PITCH = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis * normAccel.YAxis + normAccel.ZAxis * normAccel.ZAxis)) * 180.0) / M_PI;
-  int ROLL = (atan2(normAccel.YAxis, normAccel.ZAxis) * 180.0) / M_PI;
+  PITCH = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis * normAccel.YAxis + normAccel.ZAxis * normAccel.ZAxis)) * 180.0) / M_PI;
+  ROLL = (atan2(normAccel.YAxis, normAccel.ZAxis) * 180.0) / M_PI;
   int KEY_ARMING = 0;
   int KEY_LAND = 0;
   int KEY_TAKEOFF = 0;
 
   if (!isCalibrated) {
-
-    cal_cnt++;
-    if (PITCH < last_PITCH) PITCH_MIN = PITCH;
-    if (PITCH > last_PITCH) PITCH_MAX = PITCH;
-    if (ROLL < last_ROLL) ROLL_MIN = ROLL;
-    if (ROLL > last_ROLL) ROLL_MAX = ROLL;
+   
+    Serial.println("Cari limit sudut");
+    
+    if (cal_cnt == 0){
+      
+      PITCH_MIN = PITCH;
+      PITCH_MAX = PITCH;
+      ROLL_MIN = ROLL;
+      ROLL_MAX = ROLL;
+      
+      
+    }else{
+    
+    if (PITCH < PITCH_MIN ) PITCH_MIN = PITCH;
+    if (PITCH > PITCH_MAX) PITCH_MAX = PITCH;
+    if (ROLL < ROLL_MIN) ROLL_MIN = ROLL;
+    if (ROLL > ROLL_MAX) ROLL_MAX = ROLL;
+      
+    }
 
     digitalWrite(PIN_LED_FRONT, HIGH);
     digitalWrite(PIN_LED_RIGHT, HIGH);
     digitalWrite(PIN_LED_BACK, HIGH);
     digitalWrite(PIN_LED_LEFT, HIGH);
-    delay(50);
-    last_PITCH = PITCH;
-    last_ROLL = ROLL;
+    delay(100);
+    
     digitalWrite(PIN_LED_FRONT, LOW);
     digitalWrite(PIN_LED_RIGHT, LOW);
     digitalWrite(PIN_LED_BACK, LOW);
     digitalWrite(PIN_LED_LEFT, LOW);
     delay(100);
+    cal_cnt++;
     if (cal_cnt > CALIBRATE_STEP) isCalibrated = true;
 
   } else {
@@ -138,7 +159,10 @@ void loop() {
             KEY_TAKEOFF);
 
     wifiUdp.beginPacket(kRemoteIpadr, kRmoteUdpPort);
-    if (isCalibrated) wifiUdp.print(buffer);
+    if (isCalibrated) {
+      wifiUdp.print(buffer);
+      Serial.println("Sending UDP");
+    }
     wifiUdp.endPacket();
     digitalWrite(LED_BUILTIN, LOW);
     delay(100);
