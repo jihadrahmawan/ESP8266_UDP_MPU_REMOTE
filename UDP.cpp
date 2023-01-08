89762e5378aaf7b3d48b0704709b898945911bc2
Jihad Rahmawan <jihadrahmawan20@gmail.com>
2023-01-09 01:17:42 +0900

    Update UDP.cpp

--- UDP.cpp
+++ UDP.cpp
@@ -95,7 +95,8 @@ void setup() {
   Serial_setup();
   WiFi_setup();
  
- 
+  digitalWrite(LED_BUILTIN, LOW);
+  
   while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G)) {
     Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
     delay(500);
